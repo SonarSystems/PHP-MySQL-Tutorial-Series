f580e7f5a7dd1736cac1597980c7efd0d0da0f47
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-09-04 11:53:58 +0100

    Code

---
+++ Code/[7] MySQL/Connecting To A Database/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$serverName = "localhost";
+$username = "root";
+$password = "";
+
+try
+{
+    $connection = new PDO( "mysql:host=$serverName;dbname=EpicDatabase", $username, $password );
+    
+    $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    
+    echo "Connected successfully<br />";
+}
+catch( PDOException $error )
+{
+    echo "Connection failed: ".$error->getMessage( )."<br />";
+}
+
+$connection = null;
+
+?>
+
+
+
+
+
+
+
+
---
+++ Code/[7] MySQL/LIKE Property For Pattern Search/index.php
@@ -0,0 +1,40 @@
+<?php
+
+$serverName = "localhost";
+$username = "root";
+$password = "";
+
+try
+{
+    $connection = new PDO( "mysql:host=$serverName;dbname=EpicDatabase", $username, $password );
+    
+    $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    
+    echo "Connected successfully<br />";
+}
+catch( PDOException $error )
+{
+    echo "Connection failed: ".$error->getMessage( )."<br />";
+}
+
+$query = $connection->prepare( "SELECT id, username FROM Users WHERE username LIKE '%a%'" );
+$query->execute( );
+
+$result = $query->setFetchMode( PDO::FETCH_ASSOC );
+
+foreach( new RecursiveArrayIterator( $query->fetchAll( ) ) as $key => $value )
+{
+    echo $value["id"]." : ".$value["username"]."<br />";
+}
+
+$connection = null;
+
+?>
+
+
+
+
+
+
+
+
