d05e00346646b2cf285166f45510decdcce3b380
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-09-26 17:14:05 +0100

    Code

---
+++ Code/[11] Object Oriented Programming/Function Overriding/index.php
@@ -0,0 +1,61 @@
+<?php
+
+class Vehicle
+{
+    public $speed;
+    public $acceleration;
+    public $wheels;
+    
+    public function __construct( )
+    {
+        $this->speed = 10;
+        $this->acceleration = 3.5;
+        $this->wheels = 4;
+        echo "<div style='color:red;'>I have begun</div><br />";
+    }
+    
+    public function __destruct( )
+    {
+        echo "I am dead<br />";
+    }
+    
+    public function PrintAllInfo( )
+    {
+        echo "Speed is: ".$this->speed."<br />";
+        echo "Acceleration is: ".$this->acceleration."<br />";
+        echo "Number of wheels are: ".$this->wheels."<br />";
+        
+        echo "<br />";
+    }
+    
+    public function MoveForward( )
+    {
+        echo "Move forward<br />";
+    }
+}
+
+class Car extends Vehicle
+{
+    public function __construct( )
+    {
+        echo "This is the car starting<br />";
+    }
+    
+    public function OpenDoor( )
+    {
+        echo "Open Door<br />";
+    }
+}
+
+class BMW extends Car
+{
+    public function OpenDoor( )
+    {
+        echo "Door is opening<br />";
+    }
+}
+
+$bmwObj = new BMW( );
+$bmwObj->OpenDoor( );
+
+?>
