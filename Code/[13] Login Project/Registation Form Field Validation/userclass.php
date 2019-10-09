b016cce4834f99c737c79e298d8ec883a3907f1e
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-10-09 13:40:27 +0100

    Code

---
+++ Code/[13] Login Project/Registation Form Field Validation/userclass.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once( "dbclass.php" );
+
+class User
+{   
+    private $db;
+    
+    public function __construct( )
+    {
+        $this->db = new Database( "localhost", "root", "", "LoginProject" );
+    }
+    
+    public function Register( $username, $password, $confirmPassword, $email )
+    {
+        $errors = array( );
+        $valid = true;
+        
+        if ( strlen( $username ) < 6 || strlen( $username ) > 32 )
+        {
+            $valid = false;
+            array_push( $errors, "Username length should be between 6 and 32" );
+        }
+        
+        if ( strlen( $password ) < 6 || strlen( $password ) > 32 )
+        {
+            $valid = false;
+            array_push( $errors, "Password length should be between 6 and 32" );
+        }
+        else
+        {
+            if ( $password !== $confirmPassword )
+            {
+                $valid = false;
+                array_push( $errors, "Passwords do not match" );
+            }
+        }
+        
+        if ( !filter_var( $email, FILTER_VALIDATE_EMAIL ) )
+        {
+            $valid = false;
+            array_push( $errors, "Invalid email address" );
+        }
+        else
+        {
+            if ( strlen( $email ) > 100 )
+            {
+                $valid = false;
+                array_push( $errors, "Email is too long" );
+            }
+        }
+        
+        if ( $valid )
+        {
+            $fields = array
+            (
+                "username" => array( ":username" => $username ),
+                "password" => array( ":password" => $password ),
+                "email" => array( ":email" => $email ),
+            );
+            
+            $this->db->Insert( "users", $fields );
+            
+            return true;
+        }
+        else
+        {
+            return $errors;
+        }
+    }
+}
+
+?>
