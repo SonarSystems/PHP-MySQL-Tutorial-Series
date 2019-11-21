b2b8952c0a4e88f79ca18e601201ec536f34a284
Sonar Systems <SonarSystems@users.noreply.github.com>
2019-11-21 13:51:25 +0000

    Code

---
+++ Code/[15] Advanced/Include and Require Files/Header.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "This is the header<br/ >";
+
+?>
---
+++ Code/[15] Advanced/Include and Require Files/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//include "Header.php";
+require_once "Header.php";
+require_once "Header.php";
+
+echo "Main body<br />";
+
+include "Footer.php";
+
+?>
